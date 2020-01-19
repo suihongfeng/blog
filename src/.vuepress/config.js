1aa8d3bce1892e6a0d695c77da5a0387fd3fe2c0
liutianbao <liutianbao@codeus.com.cn>
2020-01-19 11:10:13 +0800

    deploy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   base: '/blog/',
   dest: 'docs',
-  title:'Bert\'s Blog',
+  title:'天宝的博客',
   description:'执着于更高的技术追求！',
   head:[
     ['link', { rel: 'icon', href: 'ltbicon.ico' }],
@@ -16,18 +16,14 @@ module.exports = {
         items: [
           { text: '环境配置', link: '/fun/env'},
           { text: 'eslint规则', link: '/fun/eslint'},
-          { text: '性能优化', link: '/fun/opt'},
+          { text: 'web性能优化', link: '/fun/opt'},
           
           { text: '公众号开发总结', link: '/fun/wechat' },
           { text: '深入浅出前端脚手架', link: '/fun/scaffold' },
-          { text: '前端基础JS系列', link: '/fun/javascript-basic' },
-          { text: '移动端性能优化(一)', link: '/fun/optimize' },
-          { text: '移动端性能优化(二)', link: '/fun/optimize2' },
-          { text: '移动端的那些坑(一)', link: '/fun/mobile-bug' },
+          { text: '移动端优化与踩坑', link: '/phone/optimize' },
           { text: 'markdown语法', link: '/fun/md' },
           { text: '全栈入门', link: '/static/1_1' },
           { text: 'JS入门', link: '/js/1' },
-          { text: 'DOM教程', link: '/dom/1' },
           { text: 'JS高级导读', link: '/dd/1-3' },
           { text: '前端进阶', link: '/fe/vue' },
           // { text: '带你面试', link: '/job/sf' },
@@ -84,11 +80,12 @@ module.exports = {
       '/skulpt/': genSidebarConfig('skulpt', ['links', '1']),
       '/scratch/': genSidebarConfig('深入scratch', ['links', '1', 'sb3']),
       '/job/': genSidebarConfig('如何面试', ['sf', 'html', 'css', 'js', 'bom', 'node', 'vue', 'mp', 'self']),
-      '/fe/': genSidebarConfig('大前端教程', ['vue', 'VueRouter', 'vuex',  'linux', 'vscode', 'node', 'code', 'suanfa', 'ngnix', 'pm2', 'ssh']),
-      '/js/': genSidebarConfig('JS入门', ['1', '2', '3', '4', '5', '6', '7', '8']),
+      '/fe/': genSidebarConfig('前端教程', ['vue', 'VueRouter', 'vuex',  'linux', 'vscode', 'node', 'code', 'suanfa', 'ngnix', 'pm2', 'ssh']),
+      '/js/': genSidebarConfig('JS入门', ['1', '2', '3', '4', '5', '6', '7', '8', '9']),
       '/dom/': genSidebarConfig('JS与浏览器', ['1']),
       '/dd/': genSidebarConfig('导读', ['1-3', '4-6', '7-9', '10-12', '13-15', '16-18', '19-21', '22-25']),
       '/static/': genSidebarConfig('全栈入门',  ['1_1', '1_2', '1_3', '2_1', '2_2', '3_1', '3_2', '3_3', '4_1', '4_2', '4_3', '5_1', '5_2', '5_3', '6_1','6_2', '6_3']),
+      '/phone/': genSidebarConfig('移动端优化与踩坑', ['optimize', 'optimize2', 'mobile-bug']),
     }
   }
 }
